32912a94763460e29f7344340c2d6ae888516278
Kamalags24 <Kamaljoags@gmail.com>
2024-08-22 17:47:13 +0200

    MAJ

--- Pyramide/Pyramide.java
+++ Pyramide/Pyramide.java
@@ -9,26 +9,26 @@ public class Pyramide {
         int rows = scanner.nextInt();
         scanner.close();
 
-        // for (int i = 1; i < rows; i++) {
-        //     // Imprimer les espaces
-        //     for (int j = rows - i; j > 0; j--) {
-        //         System.out.println(" ");
-        //     }
-
-        //     //Imprimer les étoiles 
-        //     for (int k = 1; k <= (2 * i - 1); k++) {
-        //         System.out.println("*");
-        //     }
-
-        //     //Nouvelle ligne après chaque ligne de la pyramide 
-        //     System.out.println();
-        // }
-
-
         for (int i = 1; i < rows; i++) {
+            // Imprimer les espaces
+            for (int j = rows - i; j > 0; j--) {
+                System.out.println(" ");
+            }
+
+            //Imprimer les étoiles 
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+
+            //Nouvelle ligne après chaque ligne de la pyramide 
+            System.out.println();
+        }
+
+
+        for (int i = 0; i < rows; i++) {
             //Iprimer les étoiles pour chaque ligne 
             for (int j = 1; j <= i; j++) {
-                System.out.println("* ");
+                System.out.print("* ");
             }
 
             //Passer à la ligne suivante après chaque ligne d'étoiles
